dc71cd71c660d2e6cff855a912ec0729c65d7571
tonhaoSfalcini <antoniosfalcini@gmail.com>
2021-05-27 13:48:22 -0300

    Método Delete e subrecursos

--- main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -3,6 +3,7 @@ package br.com.alura.loja.resource;
 import java.net.URI;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -13,7 +14,9 @@ import javax.ws.rs.core.Response;
 
 import com.thoughtworks.xstream.XStream;
 
+import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.modelo.Carrinho;
 import br.com.alura.loja.modelo.Projeto;
 
 @Path("/projetos")
@@ -42,5 +45,13 @@ public class ProjetoResource {
 		new ProjetoDAO().adiciona(projeto);
 		URI uri = URI.create("/projetos/"+projeto.getId());
 		return Response.created(uri).build();
+	}
+	
+	@Path("{id}")
+	@DELETE
+	public Response removeProjeto(@PathParam("id") Long id) {
+		new ProjetoDAO().remove(id);
+		
+		return Response.ok().build();
 	}
 }
